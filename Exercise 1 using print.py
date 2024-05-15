0e00a6385721a6981850a6da7bc515252940bef8
Kushagra mishra <kushagramishra4326@gmail.com>
2024-05-15 18:19:17 +0530

    Initial commit

---
+++ Exercise 1 using print.py
@@ -0,0 +1,6 @@
+# First Program -Python print function
+# It is declared like this :
+# print('what to print')
+print("First Program -Python print function")
+print("It is declared like this :")
+print("print('what to print')")
